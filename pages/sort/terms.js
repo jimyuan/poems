e38cb60846ba709297490ab699605fac6682f97b
Jim Yuan <aquarior@gmail.com>
2019-03-12 15:58:45 +0800

    继续修改

--- pages/sort/terms.js
+++ pages/sort/terms.js
@@ -1,66 +1,24 @@
 // pages/sort/terms.js
+const app = getApp()
+const keyTerms = Array.from(new Set(app.globalData.poems.map(item => item[0]))).sort()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    terms: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad (options) {
+    const grade = ['一', '二', '三', '四', '五']
+    const term = ['上', '下']
+    const terms = keyTerms.map((item, idx) => `${grade[idx / 2 | 0]}年级${term[idx & 1]}`)
+    this.setData({
+      terms
+    })
   }
 })
