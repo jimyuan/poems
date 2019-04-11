d00ed4c0da189c105df656d04a68f5629ee5b4f4
Jim Yuan <aquarior@gmail.com>
2019-04-11 17:20:36 +0800

    modify wxs

--- pages/sort/author.js
+++ pages/sort/author.js
@@ -1,11 +1,12 @@
 // pages/sort/author.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    author: Array.from(new Set(app.globalData.poems.map(item => item[3])))
   },
 
   /**
@@ -19,48 +20,6 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
--- pages/sort/terms.js
+++ pages/sort/terms.js
@@ -1,24 +1,18 @@
 // pages/sort/terms.js
 const app = getApp()
-const keyTerms = Array.from(new Set(app.globalData.poems.map(item => item[0]))).sort()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    terms: {}
+    terms: Array.from(new Set(app.globalData.poems.map(item => item[0]))).sort()
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad (options) {
-    const grade = ['一', '二', '三', '四', '五']
-    const term = ['上', '下']
-    const terms = keyTerms.map((item, idx) => `${grade[idx / 2 | 0]}年级${term[idx & 1]}`)
-    this.setData({
-      terms
-    })
+    
   }
 })
